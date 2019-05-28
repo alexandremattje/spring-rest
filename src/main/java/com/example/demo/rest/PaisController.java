edac4927a1495db42fadf519c7b58c38c462b21b
Alexandre Mattje <amattje@gmail.com>
2019-05-28 17:41:45 -0300

    l

--- src/main/java/com/example/demo/rest/PaisController.java
+++ src/main/java/com/example/demo/rest/PaisController.java
@@ -2,8 +2,6 @@ package com.example.demo.rest;
 
 import com.example.demo.dto.PaisDTO;
 import com.example.demo.model.PaisModel;
-import com.example.demo.model.repository.PaisRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.file.AccessDeniedException;
 import java.util.Collection;
 import java.util.List;
 
@@ -21,7 +18,7 @@ import java.util.List;
 public class PaisController {
 
     @Autowired
-    private PaisRepository paisModel;
+    private PaisModel paisModel;
 
     @GetMapping(path = "/listar")
     public ResponseEntity<Collection<PaisDTO>> listar() {
